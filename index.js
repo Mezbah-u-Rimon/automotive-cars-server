217073df0a51534a8096b06f5329cebb1bc0104c
Muzbah Uddin <rimonmr444@gmail.com>
2023-10-21 12:03:54 +0600

    added the myCartCollection deleted option

--- index.js
+++ index.js
@@ -94,6 +94,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/myCart/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await myCartCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
 
 
