ca5337b0e75820c63ec29b0378da998b72bf4206
Muzbah Uddin <rimonmr444@gmail.com>
2023-10-21 18:58:25 +0600

    updated delete collection in server side

--- index.js
+++ index.js
@@ -96,8 +96,8 @@ async function run() {
 
         app.delete('/myCart/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await myCartCollection.deleteOne(query)
+            const query = { _id: id };
+            const result = await myCartCollection.deleteOne(query);
             res.send(result)
         })
 
